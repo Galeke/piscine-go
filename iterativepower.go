241eeaa8e7022115b992735dbc417c07f4108161
Galeke <kossan.galym@gmail.com>
2019-10-21 11:43:58 +0600

    iter power

--- iterativepower.go
+++ iterativepower.go
@@ -11,16 +11,3 @@ func IterativePower(nb int, power int) int {
 	}
 	return result
 } 
-
-
-import (
-        "fmt"
-        piscine ".."
-)
-
-
-func main() {
-	arg1 := 4
-	arg2 := 3
-	fmt.Println(piscine.IterativePower(arg1, arg2))
-}
