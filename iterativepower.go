92f6979d4202fcd7db2079e91c93ddef4eb6f41e
Galeke <kossan.galym@gmail.com>
2019-10-21 11:55:22 +0600

    Oter pow

--- iterativepower.go
+++ iterativepower.go
@@ -4,7 +4,9 @@ func IterativePower(nb int, power int) int {
 	result := nb
 	if power > 0 {
 		for i := 1; i < power; i++ {
-			result = nb * result
+			if i < power {
+				result = nb * result
+			}
 		}
 	} else {
 		result = 0
