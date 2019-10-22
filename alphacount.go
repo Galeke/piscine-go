1023006aa7c5b418d8368b20d3f55b19f9ffdf46
Galeke <kossan.galym@gmail.com>
2019-10-22 10:28:13 +0600

    q5-1

--- alphacount.go
+++ alphacount.go
@@ -3,7 +3,7 @@ package piscine
 func AlphaCount(str string) int {
 	a := 0
 	for i := 0; i < len(str); i++ {
-		if str[i] >= 'A' && str [i] <='Z' || str[i] >= 'a' && str[i] <= 'z' {
+		if str[i] >= 'A' && str[i] <= 'Z' || str[i] >= 'a' && str[i] <= 'z' {
 			a++
 		}
 	}
