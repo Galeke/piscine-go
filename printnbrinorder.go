90133e01a64a9054d626b46bed2d13ff596cef10
Galeke <kossan.galym@gmail.com>
2019-10-22 11:56:02 +0600

    printnbrorder

---
+++ printnbrinorder.go
@@ -0,0 +1,18 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintNbrInOrder(n int) {
+	var t []int
+	if n == 0 {
+		t = append(t, 0)
+	}
+	for i := 0; n > 0; i++ {
+		t = append(t, n%10)
+		n /= 10
+	}
+
+	for i := range t {
+		z01.PrintRune(rune('0' + t[i]))
+	}
+}
