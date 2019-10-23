82f677ae63be74f6ba32ebbce516ef73ff9f9160
Galeke <kossan.galym@gmail.com>
2019-10-23 15:39:29 +0600

    q4e6

---
+++ lastrune.go
@@ -0,0 +1,14 @@
+package piscine 
+
+func LastRune(s string) rune {
+		Arr1:= []rune(s)
+		count := 0
+		for _, i := range Arr1 {
+			if i == i {
+			}
+			count++
+		
+		}
+		return Arr1[count-1]
+	}
+	
