986344cc4428ee9ba59bc5a454a9a3c81aa16b5c
Galeke <kossan.galym@gmail.com>
2019-10-23 15:54:53 +0600

    q5e7

---
+++ index.go
@@ -0,0 +1,16 @@
+package piscine 
+
+func Index(s string, toFind string) int {
+	sToRune := []rune(s)
+	toFindtoRune := []rune(toFind)
+	length := 0
+	for range toFindtoRune {
+			length++
+	}
+	for index, str := range sToRune {
+		if length > 0 && str == toFindtoRune[0] {
+			return index
+		}
+	}
+	return -1
+}
