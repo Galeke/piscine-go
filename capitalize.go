234f40520b2e306e048092e7c739b09b4b278194
Galeke <kossan.galym@gmail.com>
2019-10-23 17:28:56 +0600

    q5e11

---
+++ capitalize.go
@@ -0,0 +1,26 @@
+package piscine 
+
+func AlphNum(t rune) bool {
+	if t >= 'A' && t <= 'Z' || t >= 'a' && t <= 'z' || t >= '0' && t <= '9' {
+		return true
+	}
+	return false 
+}
+
+func Capitalize(s string) string {
+	Arr1 := []rune(s)
+	for index, letter := range Arr1 {
+		if AlphNum(letter) {
+			if index == 0 || AlphNum(Arr1[index-1]) == false {
+				if letter >= 'a' && letter <= 'z' {
+					Arr1[index] = letter - 32
+				}
+			}else{
+				if letter >= 'A' && letter <= 'Z' {
+					Arr1[index] = letter + 32
+				}
+			}
+		}
+	}
+	return string(Arr1)
+}
