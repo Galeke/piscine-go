d170ec3cfd6be08c3b7507e081fb9a8aebf7e711
Galeke <kossan.galym@gmail.com>
2019-10-23 17:13:31 +0600

    q5e9

--- toupper.go
+++ toupper.go
@@ -7,6 +7,6 @@ func ToUpper(s string) string {
 			Arr1[index] = Arr1[index] - 32 //shift down in ASCII table is so stoopid jesus
 		}
 	}
-	caps:= string(Arr1)
+	caps := string(Arr1)
 	return caps
 }
