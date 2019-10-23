1d1f55673598a70925bceeefe3565c1267b52c72
Galeke <kossan.galym@gmail.com>
2019-10-23 19:35:04 +0600

    q6e2

---
+++ printparams/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	argument := os.Args
+	for index, argument := range argument {
+		if index != 0 {
+			for _, c := range argument {
+			z01.PrintRune(c)
+			}
+		}
+		z01.PrintRune(10)
+
+	}
+}
